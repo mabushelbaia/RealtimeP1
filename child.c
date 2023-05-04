77b8e545c9bc39fb3b26028932b19033b9bc305b
Basil Mari <basilm4r0@gmail.com>
2023-05-04 14:44:10 +0300

    Child generates random number within range in range.txt and writes it to file identified by the child's pid

--- child.c
+++ child.c
@@ -4,6 +4,21 @@
 #include <sys/types.h>
 
 int main(int argc, char *argv[]) {
+    // Read range.txt and generate a random number in that range
+    FILE *fp;
+    int min, max, random;
+    fp = fopen("range.txt", "r");
+    fscanf(fp, "%d %d", &min, &max);
+    fclose(fp);
+    random = (rand() % (max - min + 1)) + min;
+
+    // Write the random number to a file named after the process id
+    char filename[20];
+    sprintf(filename, "%d.txt", getpid());
+    fp = fopen(filename, "w");
+    fprintf(fp, "%d", random);
+    fclose(fp);
+
     printf("I am a child process with PID = %d\n", getpid());
     return 0;
 }
