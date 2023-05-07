676c93e4f2eeb8e6823cafd85233739c6674dcb6
mabushelbaia <mabushelbaia@gmail.com>
2023-05-07 08:38:11 +0300

    make: added debug option

--- src/child.c
+++ src/child.c
@@ -1,67 +1,26 @@
 #include "child.h"
 
-volatile sig_atomic_t finished_flag = False;
+volatile sig_atomic_t confirmed_f = false;
 int main(int argc, char *argv[]) {
+    printf("[%d] Child process started\n", getpid());
     handler_setup(SIGUSR1, &start);
-    handler_setup(SIGUSR2, &finished);
-    while (!finished_flag) {
-        kill(getppid(), SIGUSR1); // Send SIGUSR2 to parent (Ready signal)
-    }
-    finished_flag = False;
-    kill(getppid(), SIGUSR1); // Send SIGUSR1 to parent (Ready signal)
+    handler_setup(SIGUSR2, &confirmed);
     pause();
     return 0;
 }
-void finished(int sig, siginfo_t *info, void *context) {
-    finished_flag = True;
-    printf("[%d] Received finishing signal\n", getpid());
-    exit(0);
+void confirmed(int sig, siginfo_t *info, void *context) {
+    confirmed_f = true;
+    printf("[%d] Received confirmation signal\n", getpid());
 }
+
 void start(int sig, siginfo_t *info, void *context) {
     printf("[%d] Received starting signal\n", getpid());
     int min, max;
     read_range("./txt/range.txt", &min, &max);
-    float number = generate_random_float_number(min, max);
-    write_random_float_number(getpid(), number);
-}
-void read_range(char *filename, int *min, int *max)
-{
-    FILE *fp = fopen(filename, "r");
-    if (fp == NULL)
-    {
-        perror("fopen");
-        exit(1);
+    write_random_float_number(getpid(), min, max);
+    while (!confirmed_f) {
+        kill(getppid(), SIGUSR1);
     }
-    fscanf(fp, "%d,%d", min, max);
-    fclose(fp);
+    confirmed_f = false;
 }
 
-
-float generate_random_float_number(int min, int max){
-    srand(time(NULL) + getpid());
-    return (float)rand() / (float)(RAND_MAX / max);
-}
-
-void handler_setup(int sig, void (*handler)(int, siginfo_t *, void *)) {
-	struct sigaction sa;
-	sa.sa_handler = (void (*)(int)) handler;
-	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	if (sigaction(sig, &sa, NULL) == -1) {
-		perror("sigaction");
-		exit(1);
-	}
-}
-
-void write_random_float_number(int pid, float number) {
-    char filename[20];
-    sprintf(filename, "./txt/%d.txt", pid);
-    FILE *fp = fopen(filename, "w");
-    if (fp == NULL)
-    {
-        perror("fopen");
-        exit(1);
-    }
-    fprintf(fp, "%f", number);
-    fclose(fp);
-}
